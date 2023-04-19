f8b0ee7adcad45c9417f725bfba7cce1bcb32dcf
irvingalindo <=>
2023-04-19 10:37:53 -0600

    first commit

---
+++ src/test/java/org/automation/AppTest.java
@@ -0,0 +1,14 @@
+package org.automation;
+
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertTrue;
+
+
+
+public class AppTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
