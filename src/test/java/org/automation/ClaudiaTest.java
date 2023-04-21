7e7bfc59430215bb898b99b749655bef7d58b2c3
Your Name <you@example.com>
2023-04-20 19:24:12 -0600

    add change

--- src/test/java/org/automation/ClaudiaTest.java
+++ src/test/java/org/automation/ClaudiaTest.java
@@ -9,7 +9,7 @@ public class ClaudiaTest {
     public void shouldAnswerWithTrue() {
         assertTrue(true);
         /*comentariospalabanda*/
-        /*segundo comentario UwU*/
+        /*segundo comentario m UwU*/
 
     }
 }
