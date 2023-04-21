a511098c2b30636e434d787a67f75ff865038996
Arturo Torres <arturotorres@MacBook-Air-de-Arturo.local>
2023-04-20 18:43:45 -0600

    nueva version

--- src/test/java/org/automation/ArturoTest.java
+++ src/test/java/org/automation/ArturoTest.java
@@ -1,5 +1,9 @@
 package org.automation;
 
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
 public class ArturoTest {
     @Test
     public void shouldAnswerWithTrue() {
