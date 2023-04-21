b93904703f4c8d3f00482278594b39271b772e01
mfierros103 <mfierros103@gmail.com>
2023-04-20 18:45:49 -0700

    cambios

--- src/test/java/org/automation/requestMarcela.java
+++ src/test/java/org/automation/requestMarcela.java
@@ -1,4 +1,11 @@
 package org.automation;
 
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertTrue;
+
 public class requestMarcela{
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
 }
