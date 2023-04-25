2cb3f95909096ac18852e78f4cadc89db8a0f67d
andreamg93 <andreamagana.g@gmail.com>
2023-04-25 13:06:07 -0600

    Ya lo estoy logrando :)

---
+++ src/test/java/org/automation/AndreaTest.java
@@ -0,0 +1,13 @@
+package org.automation;
+
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertTrue;
+
+public class AndreaTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+
+}
+
