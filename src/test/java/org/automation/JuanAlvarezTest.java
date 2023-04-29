2c4d0a31a6f8a7235b26f68acaadf6b3fa712a80
siderak46 <axor46@gmail.com>
2023-04-28 19:08:36 -0500

    Add a new class with 2 test

---
+++ src/test/java/org/automation/JuanAlvarezTest.java
@@ -0,0 +1,55 @@
+package org.automation;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.apache.http.HttpStatus;
+import org.testng.annotations.Test;
+import static org.hamcrest.Matchers.is;
+import static org.testng.Assert.assertEquals;
+
+public class JuanAlvarezTest {
+
+    @Test
+    public void shouldICreateMyFirstAutomationTest(){
+        //User user = new User(); RestAssured doesn't need it.
+        RestAssured.baseURI = "https://reqres.in";
+        RestAssured.basePath = "/api";
+        RestAssured
+                .given()
+                .pathParam("userId", 2)
+                .when()
+                .get("/users/{userId}")
+                .then()
+                .statusCode(HttpStatus.SC_OK)
+                .body("data.id", is(2))
+                .body("data.first_name", is("Janet"))
+                .body("data.last_name", is("Weaver"))
+                .body("data.email", is("janet.weaver@reqres.in"));
+
+    }
+    @Test
+    public void shouldValidateUserWithTestNgAssertions(){
+        RestAssured.baseURI = "https://reqres.in";
+        RestAssured.basePath = "/api";
+        Response jsonPath = RestAssured
+                .given()
+                .pathParam("userId", 2)
+                .when()
+                .get("/users/{userId}")
+                .then()
+                .extract()
+                .response();
+
+        System.out.println(jsonPath.asPrettyString());
+        int actualUserId = jsonPath.path("data.id");
+        String actualFirstName = jsonPath.path("data.first_name");
+        String actualLastName = jsonPath.path("data.last_name");
+        String actualEmail = jsonPath.path("data.email");
+    
+        assertEquals(jsonPath.statusCode(),HttpStatus.SC_OK, "Status should be 200");
+        assertEquals(actualUserId, 2, "User Id should be 2");
+        assertEquals(actualFirstName, "Janet", "Name should be Janet");
+        assertEquals(actualLastName, "Weaver", "Last name should be Weaver");
+        assertEquals(actualEmail, "janet.weaver@reqres.in", "Email should be janet.weaver@reqres.in");
+    }
+}
