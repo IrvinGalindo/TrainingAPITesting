40230963263620480435b611e9cca4f28bda55a6
KimberlySA95 <kimberly.sanare95@gmail.com>
2023-05-02 18:25:44 -0600

    add friday 2804

--- src/test/java/org/automation/KimberlyTest.java
+++ src/test/java/org/automation/KimberlyTest.java
@@ -3,19 +3,31 @@ package org.automation;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.apache.http.HttpStatus;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 
+import java.util.HashMap;
+
+import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.is;
 import static org.testng.Assert.assertEquals;
 
 public class KimberlyTest {
+
+    @BeforeClass
+    public void serUpRestAssured(){
+         RestAssured.baseURI = "https://reqres.in/";
+         RestAssured.basePath = "/api";
+    }
+
     @Test
     public void shouldAnswerWithTrue() {
-        RestAssured.baseURI = "https://reqres.in/";
-        RestAssured.basePath = "/api";
-        RestAssured
-                .given() //obtenemos nuevo resultado
+     //   RestAssured.baseURI = "https://reqres.in/";
+     //   RestAssured.basePath = "/api";
+        // RestAssured tambi´pen se opuede quitar esto y solo importarlo en el given sin punto
+                given() //obtenemos nuevo resultado
                 .pathParam("userId",2) //a ese nuevo resultado le mandamos llamar el metodo que tiene y le estamos mandando información
                 .when() //Obtenemos el objeto que regresa el when
                 .get("/users/{userId}")//extraemos el metodo get donde le extraemos información
@@ -29,14 +41,15 @@ public class KimberlyTest {
 
     @Test
     public void shouldValidateUserWithTestNgAssertions(){
-        RestAssured.baseURI = "https://reqres.in/";
-        RestAssured.basePath = "/api";
-        Response jsonPath = RestAssured
-                .given()
+    //    RestAssured.baseURI = "https://reqres.in/"; //Esto se repite arriba asì que no es necesario volverlo a poner
+    //    RestAssured.basePath = "/api";
+        Response jsonPath = given()
+                .log().all()
                 .pathParam("userId",2)
                 .when()
                 .get("/users/{userId}")
                 .then()
+                .log().all()
                 .extract()
                 .response();
 
@@ -51,7 +64,27 @@ public class KimberlyTest {
         assertEquals(actualFirstName, "Janet", "User name should be Janet");
         assertEquals(actualLastName, "Weaver","User last name should be Weaver");
         assertEquals(actualEmail, "janet.weaver@reqres.in","User email should be janet.weaver@reqres.in");
+    }
+
+    @Test
+    public void shouldValidateUserWithMap(){
+        HashMap user = new HashMap();
+        user.put("first_name", "Janet");
+        user.put("last_name", "Weaver");
+        user.put("email", "janet.weaver@reqres.in");
+        user.put("avatar", "http://reqres.in/img/faces/2-image.jpg");
+
+
+        given()
+                .log().all()
+                .pathParam("userId",2)
+                .when()
+                .get("/users/{userId}")
+                .then()
+                .log().all()
+                .body("data",is(user));
 
 
     }
+
 }
