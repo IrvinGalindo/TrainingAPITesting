f8840a7d12eb89b771ab7b13dbccdda563283fa5
Nicko1785 <nicko.aguilar85@gmail.com>
2023-05-04 16:35:04 -0600

    test update user

--- src/test/java/org/automation/Nicko/UpdateUser.java
+++ src/test/java/org/automation/Nicko/UpdateUser.java
@@ -1,8 +1,13 @@
 package org.automation.Nicko;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.apache.http.HttpStatus;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class UpdateUser {
 
@@ -13,7 +18,38 @@ public class UpdateUser {
     }
 
     @Test
-    public void userShouldBeUpdateName() {
+    public void jobShouldBeUpdateJobToSeniorTester() {
+        String originalJob = "senior tester";
+        given()
+                //.contentType("application/json")
+                .contentType(ContentType.JSON)
+                .body("{\n" +
+                        "    \"name\":  \"morpheus\",\n" +
+                        "    \"job\": \"" + originalJob + "\"\n" +
+                        "}" )
+                .when()
+                .put("/users/2")
+                .then()
+                .statusCode(HttpStatus.SC_OK)
+                .body("job", equalTo(originalJob))
+                .log().all();
+    }
+@Test
+    public void nameUserShouldBeMatch(){
+        String expectedName = "morpheus";
+        given()
+                //.contentType("application/json")
+                .contentType(ContentType.JSON)
+                .body("{\n" +
+                        "    \"name\": \"" + expectedName + "\",\n" +
+                        "    \"job\": \"senior tester\"\n" +
+                        "}" )
+                .when()
+                .put("/users/2")
+                .then()
+                .statusCode(HttpStatus.SC_OK)
+                .body("name", equalTo(expectedName))
+                .log().all();
 
     }
 }
