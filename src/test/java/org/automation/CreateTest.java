1c03579e86a4bc7708e8fb4da9e714baf3fa2cc9
Your Name <you@example.com>
2023-05-05 18:02:03 -0600

    Add this change

--- src/test/java/org/automation/CreateTest.java
+++ src/test/java/org/automation/CreateTest.java
@@ -1,11 +1,13 @@
 package org.automation;
 
-import POJOs.User;
+import POJOs.CreateUser;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.apache.http.HttpStatus;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.io.File;
@@ -19,10 +21,11 @@ import static org.hamcrest.Matchers.equalTo;
 
 public class CreateTest {
     @BeforeClass
-    public void setUp (){
+    public void setUp() {
         RestAssured.baseURI = "https://reqres.in";
         RestAssured.basePath = "/api";
     }
+
     @Test
     public void userShouldBeCreatedAndNameIsReturn() {
         String expectedName = "Galletita";
@@ -40,40 +43,46 @@ public class CreateTest {
                 .body("name", equalTo(expectedName));
     }
 
-        @Test
-                public void userShouldBeCreatedAndJobIsReturn() {
-            String expectedJob = "zion resident";
-            given()
-                    .contentType(ContentType.JSON) //Hay varios tipos de contentType, se despliegan al escribir el comando
-                    .body("{\n" +
-                            "\t\"name\": \"Galletita\",\n" +
-                            "\t\"job\" : \""+ expectedJob + "\"\n"+ // "\t\"job\" : \"zion resident\"\n" +
-                            "\t\n" +
-                            "}")
-                    .when()
-                    .post("/users")
-                    .then()
-                    .statusCode(HttpStatus.SC_CREATED)
-                    .body("job", equalTo(expectedJob));
-        }
+    @Test
+    public void userShouldBeCreatedAndJobIsReturn() {
+        String expectedJob = "zion resident";
+        given()
+                .contentType(ContentType.JSON) //Hay varios tipos de contentType, se despliegan al escribir el comando
+                .body("{\n" +
+                        "\t\"name\": \"Galletita\",\n" +
+                        "\t\"job\" : \"" + expectedJob + "\"\n" + // "\t\"job\" : \"zion resident\"\n" +
+                        "\t\n" +
+                        "}")
+                .when()
+                .post("/users")
+                .then()
+                .statusCode(HttpStatus.SC_CREATED)
+                .body("job", equalTo(expectedJob));
+    }
 
-        @Test
-    public void userShouldBeCreatedFromJson () throws IOException {
-            ObjectMapper objectMapper = new ObjectMapper();
-            File requestBody = Paths.get("src/test/resources/Claudia/CreateUser.json").toFile();
-            User user = objectMapper.readValue(requestBody,User.class);
+    @Test
+    public void userShouldBeCreatedFromJson() throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        File requestBody = Paths.get("src/test/resources/Claudia/CreateUser.json").toFile();
+        CreateUser user = objectMapper.readValue(requestBody, CreateUser.class);
 
-            String expectedJob = "zion resident";
-            given()
-                    .contentType(ContentType.JSON) //Hay varios tipos de contentType, se despliegan al escribir el comando
-                    .body(user)
-                    .when()
-                    .post("/users")
-                    .then()
-                    .statusCode(HttpStatus.SC_CREATED)
-                    .body("job", equalTo(expectedJob));
-        }
+        String expectedJob = "zion resident";
+        given()
+                .contentType(ContentType.JSON) //Hay varios tipos de contentType, se despliegan al escribir el comando
+                .body(user)
+                .when()
+                .post("/users")
+                .then()
+                .statusCode(HttpStatus.SC_CREATED)
+                .body("job", equalTo(user.getJob()));
+    }
+
+
+
+
 
 
     }
+
+
 
